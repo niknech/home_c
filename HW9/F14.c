4857c42205807630d412b0a8b2ea012ea61cc0bc
unknown <nik.nech97@gmail.com>
2025-12-11 00:00:14 +0300

    correcting F14

--- HW9/F14.c
+++ HW9/F14.c
@@ -1,9 +1,19 @@
 #include <stdio.h> 
 
+void swap (int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
  int sum_between_ab(int from, int to, int size, int a[])
  {
     int sum = 0;
     
+    if (from > to)
+        swap(&from, &to);
+
     for (int i = 0; i < size; i++)
     {
         if (a[i] >= from && a[i]<=to)
